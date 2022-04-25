915005842d31558db4fd4cda41bc21d871dddf1a
ikbalr <41229003+ikbalr@users.noreply.github.com>
2022-04-25 23:18:09 +0700

    Create volume_kerucut.py

---
+++ volume_kerucut.py
@@ -0,0 +1,5 @@
+tinggi = int(input('Berapa Tinggi Kerucut? :'))
+jari = int(input('Berapa Jari-jarinya? :'))
+hasil = float((1/3) * (22/7) * (jari**2) * tinggi)
+
+print(f'Volume Kerucut Tersebut Adalah : %0.2f' % hasil)
