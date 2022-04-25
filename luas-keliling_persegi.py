1f7b308fc9466119774a92a2676af04481686da6
ikbalr <41229003+ikbalr@users.noreply.github.com>
2022-04-25 23:19:14 +0700

    Create luas-keliling_persegi.py

---
+++ luas-keliling_persegi.py
@@ -0,0 +1,7 @@
+print("MENGHITUNG LUAS & KELILING PERSEGI")
+s = float(input("\nMasukan Panjang Sisi: "))
+luas = s**2
+keliling = 4 * s
+
+print("\nLuas Persegi \t\t:",int(luas))
+print("Keliling Persegi\t:",keliling)
