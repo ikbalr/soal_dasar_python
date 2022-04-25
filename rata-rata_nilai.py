0a520c4d4e0413ef89bd4d0aabc758d9cebf66dc
ikbalr <41229003+ikbalr@users.noreply.github.com>
2022-04-25 23:17:29 +0700

    Create rata-rata_nilai.py

---
+++ rata-rata_nilai.py
@@ -0,0 +1,6 @@
+n1 = int(input('Masukkan nilai pertama :'))
+n2 = int(input('Masukkan nilai kedua :'))
+n3 = int(input('Masukkan nilai ketiga :'))
+total = n1+n2+n3
+rata = total/3
+print(f'Rata-rata dari nilai tersebut adalah : %0.2f' % rata)
